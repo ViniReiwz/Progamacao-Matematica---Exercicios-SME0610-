a367f4cf02389538f64ce862ec987e355b544cd2
ViniReiwz <vinireisg@usp.br>
2026-05-17 16:38:15 -0300

    Ex3.3 finalizado

---
+++ Ex3.3/ex3-3_solved.py
@@ -0,0 +1,35 @@
+import pyscipopt as pcp
+
+# Recebendo a variável 'c' da entrada:
+c = float(input())
+
+# Iniciando modelagem
+model = pcp.Model()
+
+# Adicionando variáveis:
+x1 = model.addVar(name='x1',vtype='C',lb=0) # x1 pertence aos Reais não-negativos ( x1 >= 0)
+x2 = model.addVar(name='x2',vtype='I',lb=0) # x2 pertence aos Inteiros não-negativos (x2 >= 0 )
+
+# Adicionando restrições:
+model.addCons(5*x1 + x2 >= 100)
+model.addCons(x1 - x2 <= 15)
+
+# Função objetivo
+model.setObjective(c * (c*x1 + x2), sense='minimize')
+
+# Otimizando:
+model.hideOutput()
+model.optimize()
+
+# Verifica se a solução encontrada é finita
+if(model.getStatus() != 'unbounded'):
+
+    # Exibindo resultados com 2 casas decimais de precisão
+    print(round(model.getObjVal(),2))
+    print(round(model.getVal(x1),2))
+    print(round(model.getVal(x2),2))
+
+# Solução ilimitada
+else:
+    print('ILIMITADO')
+
